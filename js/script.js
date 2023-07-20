7f24e22c2ad2838a859a601ebecee1b328181800
pro-mannan <promasudbd@gmail.com>
2023-07-20 15:00:48 +0600

    Home Slider Complete

--- js/script.js
+++ js/script.js
@@ -8,21 +8,23 @@
 (function ($) {
  
 
- 
+//  hero Slider
 
-  //Hero Slider
-  $('.hero-slider').slick({
-    // autoplay: true,
-    infinite: true,
-    arrows: true,
-    prevArrow: '<button type=\'button\' class=\'heroSliderArrow prevArrow tf-ion-chevron-left\'></button>',
-    nextArrow: '<button type=\'button\' class=\'heroSliderArrow nextArrow tf-ion-chevron-right\'></button>',
-    dots: true,
-    autoplaySpeed: 7000,
-    pauseOnFocus: false,
-    pauseOnHover: false
-  });
-  $('.hero-slider').slickAnimation();
+$('.hero-slider').slick({
+  // autoplay:true,
+  infinite: true,
+  arrows: true,
+  prevArrow: '<button type=\'button\' class=\'heroSliderArrow prevArrow tf-ion-chevron-left\'></button>',
+  nextArrow: '<button type=\'button\' class=\'heroSliderArrow nextArrow tf-ion-chevron-right\'></button>',
+  dots: true,
+  autoplaySpeed: 7000,
+  pauseOnFocus: false,
+  pauseOnHover: false
+});
+
+$('.hero-slider').slickAnimation();
+
+
 
 
 })(jQuery);
