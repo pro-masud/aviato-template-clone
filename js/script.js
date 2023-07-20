494a5c1f6118813e46493c5ef0da1ea69aa845d4
pro-mannan <promasudbd@gmail.com>
2023-07-20 13:05:25 +0600

    project setup

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
+
+(function ($) {
+ 
+
+ 
+
+  //Hero Slider
+  $('.hero-slider').slick({
+    // autoplay: true,
+    infinite: true,
+    arrows: true,
+    prevArrow: '<button type=\'button\' class=\'heroSliderArrow prevArrow tf-ion-chevron-left\'></button>',
+    nextArrow: '<button type=\'button\' class=\'heroSliderArrow nextArrow tf-ion-chevron-right\'></button>',
+    dots: true,
+    autoplaySpeed: 7000,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+  $('.hero-slider').slickAnimation();
+
+
+})(jQuery);
